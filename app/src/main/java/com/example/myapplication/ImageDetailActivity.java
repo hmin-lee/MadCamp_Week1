d7cfdce7b751f226ffda6b520e7933227ff6aedf
JongWoo Park <jjongwoo.dev@gmail.com>
2020-07-10 18:28:12 +0900

    feature: image_detail의 설명 부분 화면 추가
    버튼을 누르면 설명 UP, 다시 누르면 Down

--- app/src/main/java/com/example/myapplication/ImageDetailActivity.java
+++ app/src/main/java/com/example/myapplication/ImageDetailActivity.java
@@ -3,11 +3,24 @@ package com.example.myapplication;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 
 public class ImageDetailActivity extends Activity {
+    boolean isPageOpen = false;
+
+    Button button;
+    LinearLayout slideDesc;
+
+    Animation translateLeftAnim;
+    Animation translateRightAnim;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +33,50 @@ public class ImageDetailActivity extends Activity {
         ImageView imageView = findViewById(R.id.image_detail);
         imageView.setImageResource(galleryAdapter.thumbImages[position]);
 
+        slideDesc = findViewById(R.id.image_desc);
+        translateLeftAnim = AnimationUtils.loadAnimation(this, R.anim.translate_up);
+        translateRightAnim = AnimationUtils.loadAnimation(this, R.anim.translate_down);
 
+        SlidingPageAnimationListener animationListener = new SlidingPageAnimationListener();
+        translateLeftAnim.setAnimationListener(animationListener);
+        translateRightAnim.setAnimationListener(animationListener);
+
+        button = findViewById(R.id.image_button);
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View view) {
+                if (isPageOpen) {
+                    slideDesc.startAnimation(translateRightAnim);
+                } else {
+                    slideDesc.setVisibility(View.VISIBLE);
+                    slideDesc.startAnimation(translateLeftAnim);
+                }
+            }
+        });
+    }
+
+    private class SlidingPageAnimationListener implements Animation.AnimationListener {
+        public void onAnimationEnd(Animation animation) {
+            if (isPageOpen) {
+                slideDesc.setVisibility(View.INVISIBLE);
+
+                button.setText("Open");
+                isPageOpen = false;
+            } else {
+                button.setText("Close");
+                isPageOpen = true;
+            }
+        }
+
+        @Override
+        public void onAnimationStart(Animation animation) {
+
+        }
+
+        @Override
+        public void onAnimationRepeat(Animation animation) {
+
+        }
     }
 }
