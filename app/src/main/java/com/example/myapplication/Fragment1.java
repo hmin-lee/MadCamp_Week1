785cc0fadecc19537bba6e66c795d8c0b4806f8a
IamHyemin <heyhyemin@kaist.ac.kr>
2020-07-11 15:37:02 +0900

    대문자 검색 가능

--- app/src/main/java/com/example/myapplication/Fragment1.java
+++ app/src/main/java/com/example/myapplication/Fragment1.java
@@ -211,7 +211,7 @@ public class Fragment1 extends Fragment {
             // 리스트의 모든 데이터를 검색한다.
             for(int i = 0;i < phonenum2.size(); i++)
             {
-                if (phonenum2.get(i).getUserName().toLowerCase().contains(charText))
+                if (phonenum2.get(i).getUserName().toLowerCase().contains(charText.toLowerCase()))
                 {
                     phonenum.add(phonenum2.get(i));
                 }
