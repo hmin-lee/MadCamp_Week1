1793c53c677885114251eb2519bf03576e40c9d9
IamHyemin <heyhyemin@kaist.ac.kr>
2020-07-11 16:55:39 +0900

    Update Fragment1.java

--- app/src/main/java/com/example/myapplication/Fragment1.java
+++ app/src/main/java/com/example/myapplication/Fragment1.java
@@ -175,8 +175,6 @@ public class Fragment1 extends Fragment {
             }
             @Override
             public void afterTextChanged(Editable editable) {
-                // input창에 문자를 입력할때마다 호출된다.
-                // search 메소드를 호출한다.
                 String text = editSearch.getText().toString();
                 search(text);
             }
