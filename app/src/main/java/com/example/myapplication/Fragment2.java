d7334e042205288abd66f997ccb2ff8d0f8fc030
JongWoo Park <jjongwoo.dev@gmail.com>
2020-07-14 00:01:04 +0900

    feature: 저장소에 있는 사진 파일 불러오기

--- app/src/main/java/com/example/myapplication/Fragment2.java
+++ app/src/main/java/com/example/myapplication/Fragment2.java
@@ -1,27 +1,41 @@
 package com.example.myapplication;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
+import androidx.annotation.Nullable;
+import androidx.core.content.FileProvider;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Date;
 
 public class Fragment2 extends Fragment implements GalleryRecyclerAdapter.OnGalleryListener {
     private static final String TAG = "Fragment2_1";
-    public static ArrayList<Integer> mImages;
+    public static ArrayList< String> mImages;
     Context myContext;
+
+    // For Using Camera Activity
+    static final int REQUEST_IMAGE_CAPTURE = 101;
 
     public Fragment2() {
         // Required empty public constructor
@@ -34,20 +48,27 @@ public class Fragment2 extends Fragment implements GalleryRecyclerAdapter.OnGall
         return fragment;
     }
 
+    private ArrayList<String> getImagesFromStorage() {
+        ArrayList<String> res = new ArrayList<>();
+        try {
+            File imagePath = new File(myContext.getFilesDir(), "images");
+            for (File s : imagePath.listFiles()) {
+                Log.d(TAG, "onClick: string:" + s.getPath());
+                if (new File(s.getPath()).exists()) {
+                    Log.d(TAG, "onClick: EXISTS!!");
+                    res.add(s.getPath());
+                } else
+                    Log.d(TAG, "onClick: NOT_EXISTS!!");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return res;
+    }
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        mImages = new ArrayList<>();
-        Integer[] tmp = {
-                R.drawable.img1, R.drawable.img2, R.drawable.img3,
-                R.drawable.img4, R.drawable.img5, R.drawable.img6,
-                R.drawable.img7, R.drawable.img8, R.drawable.img9,
-                R.drawable.img10, R.drawable.img11, R.drawable.img12,
-                R.drawable.img13, R.drawable.img1, R.drawable.img2, R.drawable.img3,
-                R.drawable.img4, R.drawable.img5, R.drawable.img6,
-                R.drawable.img7, R.drawable.img8, R.drawable.img9,
-        };
-        Collections.addAll(mImages, tmp);
     }
 
 
@@ -57,6 +78,7 @@ public class Fragment2 extends Fragment implements GalleryRecyclerAdapter.OnGall
         // Inflate the layout for this fragment
         View myView = inflater.inflate(R.layout.fragment_2, container, false);
         myContext = getContext();
+        mImages = getImagesFromStorage();
 
         initRecyclerView(myView);
 
@@ -82,9 +104,10 @@ public class Fragment2 extends Fragment implements GalleryRecyclerAdapter.OnGall
     }
 
     @Override
-    public void onGalleryClick(int position) {
+    public void onGalleryClick(int position, ArrayList<String> mImages) {
         Intent intent = new Intent(myContext, ImageDetailActivity.class);
         intent.putExtra("id", position);
+        intent.putExtra("images",  mImages);
         startActivity(intent);
     }
 
@@ -93,8 +116,82 @@ public class Fragment2 extends Fragment implements GalleryRecyclerAdapter.OnGall
         @Override
         public void onClick(View view) {
             Toast.makeText(getContext(), "FAB 클릭!! 구현 안함...ㅎ", Toast.LENGTH_SHORT).show();
+            try {
+//                dispatchTakePictureIntent();
+                File imagePath = new File(myContext.getFilesDir(), "images");
+//                Log.d(TAG, "onClick: FAB 클릭"+file.toString());
+                for (File s : imagePath.listFiles()) {
+                    Log.d(TAG, "onClick: string:" + s.getPath());
+                    if (new File(s.getPath()).exists())
+                        Log.d(TAG, "onClick: EXISTS!!");
+                    else
+                        Log.d(TAG, "onClick: NOT_EXISTS!!");
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
+    String currentPhotoPath;
 
+    private File createImageFile() throws IOException {
+        // Create an Image File name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = myContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    static final int REQUEST_TAKE_PHOTO = 111;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(myContext.getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+                Log.d(TAG, "dispatchTakePictureIntent: Error!!!");
+                ex.printStackTrace();
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(myContext,
+                        "com.example.myapplication.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
+            }
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch (requestCode) {
+            case REQUEST_TAKE_PHOTO:
+                if (resultCode == Activity.RESULT_OK && data.hasExtra("data")) {
+                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                    if (bitmap != null) {
+                        Toast.makeText(myContext, "onActivityResult: 이미지 파일 가져왔음", Toast.LENGTH_SHORT).show();
+
+                    }
+                }
+                break;
+        }
+    }
 }
--- app/src/main/java/com/example/myapplication/ImageDetailPagerAdapter.java
+++ app/src/main/java/com/example/myapplication/ImageDetailPagerAdapter.java
@@ -8,22 +8,24 @@ import androidx.fragment.app.FragmentPagerAdapter;
 import java.util.ArrayList;
 
 public class ImageDetailPagerAdapter extends FragmentPagerAdapter {
-    public Integer[] thumbImages = {
-            R.drawable.img1, R.drawable.img2, R.drawable.img3,
-            R.drawable.img4, R.drawable.img5, R.drawable.img6,
-            R.drawable.img7, R.drawable.img8, R.drawable.img9,
-            R.drawable.img10, R.drawable.img11, R.drawable.img12,
-            R.drawable.img13, R.drawable.img1, R.drawable.img2, R.drawable.img3,
-            R.drawable.img4, R.drawable.img5, R.drawable.img6,
-            R.drawable.img7, R.drawable.img8, R.drawable.img9,
-    };
+    public ArrayList<String> thumbImages;
+//    public Integer[] thumbImages = {
+//            R.drawable.img1, R.drawable.img2, R.drawable.img3,
+//            R.drawable.img4, R.drawable.img5, R.drawable.img6,
+//            R.drawable.img7, R.drawable.img8, R.drawable.img9,
+//            R.drawable.img10, R.drawable.img11, R.drawable.img12,
+//            R.drawable.img13, R.drawable.img1, R.drawable.img2, R.drawable.img3,
+//            R.drawable.img4, R.drawable.img5, R.drawable.img6,
+//            R.drawable.img7, R.drawable.img8, R.drawable.img9,
+//    };
     private ArrayList<Fragment> items;
 
-    public ImageDetailPagerAdapter(@NonNull FragmentManager fm) {
+    public ImageDetailPagerAdapter(@NonNull FragmentManager fm, ArrayList<String> mImages) {
         super(fm);
         items = new ArrayList<>();
-        for (int i = 0; i < thumbImages.length; i++) {
-            ImageInfo imageInfo = new ImageInfo.Builder(thumbImages[i]).setTitle("제목" + i + ".jpg").setDatetime("2020.07.11").build();
+        thumbImages = mImages;
+        for (int i = 0; i < thumbImages.size(); i++) {
+            ImageInfo imageInfo = new ImageInfo.Builder(thumbImages.get(i)).setTitle("제목" + i + ".jpg").setDatetime("2020.07.11").build();
             items.add(new ImageDetailFragment(imageInfo));
         }
 
