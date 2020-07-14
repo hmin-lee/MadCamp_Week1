c2b24240813ea35d294e12e75e1004d4eccc3837
IamHyemin <heyhyemin@kaist.ac.kr>
2020-07-14 18:20:34 +0900

    tab 선택 icon 색 변경

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -37,13 +37,33 @@ public class MainActivity extends AppCompatActivity {
         tab.setupWithViewPager(vp);
 
         ArrayList<Integer> images = new ArrayList<>();
-        images.add(R.drawable.phone_logo);
-        images.add(R.drawable.gallery_logo);
-        images.add(R.drawable.diary_logo);
+        images.add(R.drawable.icon_phone);
+        images.add(R.drawable.icon_gallery);
+        images.add(R.drawable.icon_diary);
+
 
         for(int i=0; i<3; i++){
             tab.getTabAt(i).setIcon(images.get(i));
         }
+
+        Objects.requireNonNull(tab.getTabAt(0).getIcon()).setColorFilter(Color.parseColor("#8E4DEA"), PorterDuff.Mode.SRC_ATOP);
+        Objects.requireNonNull(tab.getTabAt(1).getIcon()).setColorFilter(Color.parseColor("#a8a8a8"), PorterDuff.Mode.SRC_ATOP);
+        Objects.requireNonNull(tab.getTabAt(2).getIcon()).setColorFilter(Color.parseColor("#a8a8a8"), PorterDuff.Mode.SRC_ATOP);
+
+        tab.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                tab.getIcon().setColorFilter(Color.parseColor("#8E4DEA"), PorterDuff.Mode.SRC_ATOP);
+            }
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+                tab.getIcon().setColorFilter(Color.parseColor("#a8a8a8"), PorterDuff.Mode.SRC_ATOP);
+            }
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 
 
